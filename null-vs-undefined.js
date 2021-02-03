5e3d359b8ac3e5d7c6c54ac790fd32f85a6b8d02
Jobeda Nur <jobedanur265@gmail.com>
2021-02-03 19:19:47 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let age;
+// console.log(age);
+
+// function a(num1, num2){
+//     num1+num2;
+// }
+
+// let b=a(45,25);
+// console.log(b);
+
+// function a(num1, num2){
+//    return num2;
+// }
+
+// let b=a(45);
+// console.log(b);
+
+
