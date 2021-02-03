3579bd29145147986da7d6885ebab2557bdc5a5f
Jobeda Nur <jobedanur265@gmail.com>
2021-02-03 19:48:17 +0600

    map

---
+++ map-filter.js
@@ -0,0 +1,20 @@
+const numbers = [3,4,5,6,7,8];
+
+
+// const output = numbers.map(function(element){
+//     return element*element;
+// });
+// console.log(output);
+
+// const result = numbers.map(x => x * x );
+// console.log(result);
+
+
+// const bigger = numbers.filter(function(x){
+//     return x>4;
+// });
+// console.log(bigger);
+
+
+const bigger = numbers.filter(x=> x>4);
+console.log(bigger);
