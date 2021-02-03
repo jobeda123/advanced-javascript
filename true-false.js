239ecc63b259f4096e975fb224f9a04a3caa935d
Jobeda Nur <jobedanur265@gmail.com>
2021-02-03 19:09:29 +0600

    2nd commit

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy:
+// 0
+// empty string >>> ""
+// undefined
+// null
+// NaN
+// false
+
+// truthy:
+// "0"
+// " " >>>> white space
+// {}, []
+
+
+
+const age = "";
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
